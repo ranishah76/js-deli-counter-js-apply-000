0cdbba8b2c914d59ad0954e632cbc65fdb5bf60c
Rani <rani76@gmail.com>
2018-01-14 11:52:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = []
+
+function takeANumber(line, name) {
+  line.push(name)
+return `Hello, ${name}. You are number ${line.length} in line.`
+}
+
+
+function nowServing() {
+  if (line.length == 0) {
+    return "There is nobody waiting to be served!"
+  }
+
+return `Currently serving ${line.shift()}``
+}
+
+function currentLine (line) {
+const numbersandNames = []
+
+  if (line == 0) {
+    return "The line is currently empty.";
+  }
+
+for (var i = 0; i < line.length; i++) {
+  lineNum.push('${i+1}.${line[i]}')
+}
+
+    return "The line is currently:" + lineNum
+};
+  }
+}
